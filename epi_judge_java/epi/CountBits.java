b0a427427896aa3f166089c000862dbc7c44de83
James Onyango <jamesony@thoughtworks.com>
2018-06-16 15:44:05 -0700

    [1] solves count bits

--- epi_judge_java/epi/CountBits.java
+++ epi_judge_java/epi/CountBits.java
@@ -5,8 +5,12 @@ public class CountBits {
   @EpiTest(testDataFile = "count_bits.tsv")
 
   public static short countBits(int x) {
-    // TODO - you fill in here.
-    return 0;
+   short bitCount = 0;
+   while (x > 0) {
+	bitCount += x & 1;
+     	x >>>= 1;
+   } 
+    return bitCount;
   }
 
   public static void main(String[] args) {
