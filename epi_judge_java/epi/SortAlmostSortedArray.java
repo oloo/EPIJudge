c05cddbe6205174812dd481b0a6672c95a3204fa
James Onyango <jamesony@thoughtworks.com>
2018-07-29 23:09:02 -0700

    add min heap solution for SortAlmostSortedArray

--- epi_judge_java/epi/SortAlmostSortedArray.java
+++ epi_judge_java/epi/SortAlmostSortedArray.java
@@ -2,13 +2,32 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.PriorityQueue;
+
 public class SortAlmostSortedArray {
 
   public static List<Integer>
   sortApproximatelySortedData(Iterator<Integer> sequence, int k) {
     // TODO - you fill in here.
-    return null;
+    List<Integer> values = new ArrayList<Integer>();
+    PriorityQueue<Integer> heap = new PriorityQueue<>(k);
+    for(int index=0; index < k; index++) {
+        if (sequence.hasNext()) {
+		heap.add(sequence.next());
+	}
+    }
+
+    while(sequence.hasNext()) {
+	heap.add(sequence.next());
+        values.add(heap.poll());
+    }
+
+    while(heap.size() > 0) {
+        values.add(heap.poll());
+    }
+  
+  return values;
   }
   @EpiTest(testDataFile = "sort_almost_sorted_array.tsv")
   public static List<Integer>
